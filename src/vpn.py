210c4deb1cacba20c9fccd2be8dd441c321a9b8e
Alex <alexsierraalcala@gmail.com>
2023-12-18 19:57:30 -0500

    saving data

--- src/vpn.py
+++ src/vpn.py
@@ -5,9 +5,9 @@ from vpn_core import VPNBody, UserClient, NetWorkProtocol, VPNRule
 
 class VPN:
     def __init__(self, protocol: NetWorkProtocol) -> None:
-        self.__users = VPN.__read_data('data/users.json')
+        self.__users = VPN.__read_users()
+        self.__rules = VPN.__read_rules()
         self.protocol = protocol
-        self.rules = []
 
     def create_user(self, user: UserClient):
         if any(user.user == i.user for i in self.__users):
@@ -18,7 +18,7 @@ class VPN:
         user.id = len(self.__users)
         self.__users.append(user)
 
-        self.__save_data('data/users.json')
+        self.__save_users()
 
         print('User registered\n')
 
@@ -39,7 +39,7 @@ class VPN:
                 new_users.append(i)
 
         self.__users = new_users
-        self.__save_data('data/users.json')
+        self.__save_users()
 
         print('User removed\n')
 
@@ -58,18 +58,27 @@ class VPN:
         print('VPN stopped\n')
 
     def add_rule(self, rule: VPNRule):
-        rule.id = len(self.rules)
-        self.rules.append(rule)
+        if any(rule.name == i.name for i in self.__rules):
+            print('There is a rule with that name\n')
+
+            return
+        
+        rule.id = len(self.__rules)
+        self.__rules.append(rule)
+
+        self.__save_rules()
 
         print('Rule added\n')
 
     def show_rules(self):
-        for i in self.rules:
-            print(f'Id: {i.id} Name: {i.name}')
+        for i in self.__rules:
+            _type = 'VLAN Restriction' if i._type == 0 else 'User Restriction'
+            # e_id = f'id_vlan: {i.e_id}' if i._type == 0 else f'user_id: {i.e_id}'
+            print(f'Id: {i.id} Name: {i.name} Type: {_type}')
         print()
 
     def remove_rule(self, rule_id: int):
-        if rule_id < 0 or rule_id >= len(self.rules):
+        if rule_id < 0 or rule_id >= len(self.__rules):
             print('Rule not found\n')
 
             return
@@ -77,14 +86,15 @@ class VPN:
         new_rules = []
         ind = 0
 
-        for i in self.rules:
+        for i in self.__rules:
             if i.id != rule_id:
                 i.id = ind
                 ind += 1
 
                 new_rules.append(i)
 
-        self.rules = new_rules
+        self.__rules = new_rules
+        self.__save_rules()
 
         print('Rule removed\n')
 
@@ -103,7 +113,7 @@ class VPN:
 
             return
 
-        for i in self.rules:
+        for i in self.__rules:
             if not i.check(user, body):
                 print(f'Rule {i.name} blocked\n')
 
@@ -112,7 +122,9 @@ class VPN:
         self.protocol.send(body.data, (body.dest_ip, body.dest_port))
 
     @staticmethod
-    def __read_data(path: str):
+    def __read_users():
+        path: str = 'data/users.json'
+
         if not os.path.exists(path):
             return []
 
@@ -125,8 +137,37 @@ class VPN:
         except:
             return []
 
-    def __save_data(self, path: str):
+    def __save_users(self):
+        path: str = 'data/users.json'
 
         file = open(path, 'w')
         json.dump(self.__users,  file, default=lambda o: o.__dict__)
+        file.close()
+
+    @staticmethod
+    def __read_rules():
+        path: str = 'data/rules.json'
+
+        if not os.path.exists(path):
+            return []
+
+        try:
+            file = open(path, 'r')
+            data = json.load(file)
+            file.close()
+
+            return [VPNRule.dict_to_obj(i) for i in data]
+        except:
+            return []
+
+    def __save_rules(self):
+        path: str = 'data/rules.json'
+
+        file = open(path, 'w')
+        json.dump(self.__rules,  file, default=lambda o: {
+            'name': o.name,
+            'type': o._type,
+            'ip': o.ip,
+            'e_id': o.e_id
+        })
         file.close()
